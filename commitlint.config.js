744a7ab8dee33046e97b63e4cc34bdb3262ebc80
Frank Qobi <qobitek@gmail.com>
2022-11-03 22:40:41 +0100

    feat: added messages to readme.md

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,19 @@
 module.exports = { 
     extends: ["@commitlint/config-conventional"],
     rules: {
-        "type-enum": [2, "always", ["fix","feat","build","chore","ci","docs","style","refactor","perf","test","revert","hot-fix"]]
+        "type-enum": [2, "always", [
+            "fix",
+            "feat",
+            "build",
+            "chore",
+            "ci",
+            "docs",
+            "style",
+            "refactor",
+            "perf",
+            "test",
+            "revert",
+            "hot-fix"
+        ]]
     }
 }
